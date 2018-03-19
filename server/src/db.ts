6d9663565366b96339bb39436d559163cc6869e0
Wouter de Vos <wouter@codaisseur.com>
2018-03-19 10:29:35 +0100

    :blowfish:

--- server/src/db.ts
+++ server/src/db.ts
@@ -7,34 +7,34 @@ import { Player, Game } from './games/entities'
 
 class CustomNamingStrategy extends DefaultNamingStrategy implements NamingStrategyInterface {
 
-    tableName(targetName: string, userSpecifiedName: string): string {
-        return userSpecifiedName ? userSpecifiedName : snakeCase(targetName) + 's';
-    }
+  tableName(targetName: string, userSpecifiedName: string): string {
+    return userSpecifiedName ? userSpecifiedName : snakeCase(targetName) + 's';
+  }
 
-    columnName(propertyName: string, customName: string, embeddedPrefixes: string[]): string {
-        return snakeCase(embeddedPrefixes.concat(customName ? customName : propertyName).join("_"));
-    }
+  columnName(propertyName: string, customName: string, embeddedPrefixes: string[]): string {
+    return snakeCase(embeddedPrefixes.concat(customName ? customName : propertyName).join("_"));
+  }
 
-    columnNameCustomized(customName: string): string {
-        return customName;
-    }
+  columnNameCustomized(customName: string): string {
+    return customName;
+  }
 
-    relationName(propertyName: string): string {
-        return snakeCase(propertyName);
-    }
+  relationName(propertyName: string): string {
+    return snakeCase(propertyName);
+  }
 }
 
 export default () =>
-    createConnection({
-        type: "postgres",
-        url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
-        entities: [
-          User,
-          Player,
-          Game
-        ],
-        synchronize: true,
-        logging: true,
-        namingStrategy: new CustomNamingStrategy()
-    })
-        .then(_ => console.log('Connected to Postgres with TypeORM'))
+  createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
+    entities: [
+      User,
+      Player,
+      Game
+    ],
+    synchronize: true, // careful with this in production!
+    logging: true,
+    namingStrategy: new CustomNamingStrategy()
+  })
+    .then(_ => console.log('Connected to Postgres with TypeORM'))
