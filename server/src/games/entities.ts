0a84b62a9a12b0895a9f46904e5f65b8dbf02129
urbbru <timothyd28@hotmail.com>
2018-09-19 16:46:23 +0200

    HQ also object

--- server/src/games/entities.ts
+++ server/src/games/entities.ts
@@ -2,7 +2,7 @@ import { BaseEntity, PrimaryGeneratedColumn, Column, Entity, Index, OneToMany, M
 import User from '../users/entity'
 
 export type Symbol = 'red' | 'blue'
-export type Base = 'HQ'
+export type Base = {name:'HQ', health:20, team:'red' | 'blue'}
 
 // export type Team = 'red' | 'blue'
 export type Unit = {
@@ -34,7 +34,7 @@ type Status = 'pending' | 'started' | 'finished'
 
 const emptyRow: Row = [null, null, null, null, null, null]
 const row1: Row = [{name: 'john',team: 'red',health: 10,type: 'infantry'}, null, null, null, null, {name: 'jane',team: 'blue',health: 10,type: 'infantry'}]
-const row2: Row = ['HQ', {name: 'john',team: 'red',health: 10,type: 'infantry'}, null, null, {name: 'jane',team: 'blue',health: 10,type: 'infantry'}, 'HQ']
+const row2: Row = [{name:'HQ', health:20, team:'red'}, {name: 'john',team: 'red',health: 10,type: 'infantry'}, null, null, {name: 'jane',team: 'blue',health: 10,type: 'infantry'}, {name:'HQ', health:20, team:'blue'}]
 const emptyBoard: Board = [ emptyRow, row1, row2, row1, emptyRow, emptyRow ]
 
 @Entity()
