c69316b2689e4d525d100821291556dff1198282
bridgetcuevas <bridgetmichele@gmail.com>
2019-05-01 10:49:17 +0200

    calculate winner game logic/tests

--- server/src/games/entities.ts
+++ server/src/games/entities.ts
@@ -1,55 +1,70 @@
-import { BaseEntity, PrimaryGeneratedColumn, Column, Entity, Index, OneToMany, ManyToOne } from 'typeorm'
-import User from '../users/entity'
+import {
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  Index,
+  OneToMany,
+  ManyToOne
+} from "typeorm";
+import User from "../users/entity";
 
-export type Symbol = 'x' | 'o'
-export type Row = [ Symbol | null, Symbol | null, Symbol | null ]
-export type Board = [ Row, Row, Row ]
+export type Symbol = "x" | "o";
+export type Row = [
+  Symbol | null,
+  Symbol | null,
+  Symbol | null,
+  Symbol | null,
+  Symbol | null,
+  Symbol | null,
+  Symbol | null
 
-type Status = 'pending' | 'started' | 'finished'
+];
+export type Board = [Row, Row, Row, Row, Row, Row];
 
-const emptyRow: Row = [null, null, null]
-const emptyBoard: Board = [ emptyRow, emptyRow, emptyRow ]
+type Status = "pending" | "started" | "finished";
+
+const emptyRow: Row = [null, null, null, null, null, null, null];
+const emptyBoard: Board = [emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow];
 
 @Entity()
 export class Game extends BaseEntity {
-
   @PrimaryGeneratedColumn()
-  id?: number
+  id?: number;
 
-  @Column('json', {default: emptyBoard})
-  board: Board
+  @Column("json", { default: emptyBoard })
+  board: Board;
 
-  @Column('char', {length:1, default: 'x'})
-  turn: Symbol
+  @Column("char", { length: 1, default: "x" })
+  turn: Symbol;
 
-  @Column('char', {length:1, nullable: true})
-  winner: Symbol
+  @Column("char", { length: 1, nullable: true })
+  winner: Symbol;
 
-  @Column('text', {default: 'pending'})
-  status: Status
+  @Column("text", { default: "pending" })
+  status: Status;
 
   // this is a relation, read more about them here:
   // http://typeorm.io/#/many-to-one-one-to-many-relations
-  @OneToMany(_ => Player, player => player.game, {eager:true})
-  players: Player[]
+  @OneToMany(_ => Player, player => player.game, { eager: true })
+  players: Player[];
 }
 
 @Entity()
-@Index(['game', 'user', 'symbol'], {unique:true})
+@Index(["game", "user", "symbol"], { unique: true })
 export class Player extends BaseEntity {
-
   @PrimaryGeneratedColumn()
-  id?: number
+  id?: number;
 
   @ManyToOne(_ => User, user => user.players)
-  user: User
+  user: User;
 
   @ManyToOne(_ => Game, game => game.players)
-  game: Game
+  game: Game;
 
-  @Column('char', {length: 1})
-  symbol: Symbol
+  @Column("char", { length: 1 })
+  symbol: Symbol;
 
-  @Column('integer', { name: 'user_id' })
-  userId: number
+  @Column("integer", { name: "user_id" })
+  userId: number;
 }
--- server/src/games/logic.ts
+++ server/src/games/logic.ts
@@ -1,53 +1,83 @@
-import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator'
-import { Board, Symbol, Row } from './entities'
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface
+} from "class-validator";
+import { Board, Symbol, Row } from "./entities";
 
 @ValidatorConstraint()
 export class IsBoard implements ValidatorConstraintInterface {
-
   validate(board: Board) {
-    const symbols = [ 'x', 'o', null ]
-    return board.length === 3 &&
-      board.every(row =>
-        row.length === 3 &&
-        row.every(symbol => symbols.includes(symbol))
+    const symbols = ["x", "o", null];
+    return (
+      board.length === 6 &&
+      board.every(
+        row => row.length === 7 && row.every(symbol => symbols.includes(symbol))
       )
+    );
   }
 }
 
-export const isValidTransition = (playerSymbol: Symbol, from: Board, to: Board) => {
+export const isValidTransition = (
+  playerSymbol: Symbol,
+  from: Board,
+  to: Board
+) => {
   const changes = from
-    .map(
-      (row, rowIndex) => row.map((symbol, columnIndex) => ({
-        from: symbol, 
+    .map((row, rowIndex) =>
+      row.map((symbol, columnIndex) => ({
+        from: symbol,
         to: to[rowIndex][columnIndex]
       }))
     )
-    .reduce((a,b) => a.concat(b))
-    .filter(change => change.from !== change.to)
+    .reduce((a, b) => a.concat(b))
+    .filter(change => change.from !== change.to);
 
-  return changes.length === 1 && 
-    changes[0].to === playerSymbol && 
+  return (
+    changes.length === 1 &&
+    changes[0].to === playerSymbol &&
     changes[0].from === null
-}
+  );
+};
 
-export const calculateWinner = (board: Board): Symbol | null =>
+export const calculateWinner = (board: Board): Symbol | null => {
+  let winning: Symbol[] = [];
+
   board
-    .concat(
-      // vertical winner
-      [0, 1, 2].map(n => board.map(row => row[n])) as Row[]
-    )
-    .concat(
-      [
-        // diagonal winner ltr
-        [0, 1, 2].map(n => board[n][n]),
-        // diagonal winner rtl
-        [0, 1, 2].map(n => board[2-n][n])
-      ] as Row[]
-    )
-    .filter(row => row[0] && row.every(symbol => symbol === row[0]))
-    .map(row => row[0])[0] || null
+    .concat([0, 1, 2, 3, 4, 5, 6].map(n => board.map(row => row[n])) as Row[])
+    .concat([
+      // diagonal winner ltr
+      [0, 1, 2, 3, 4, 5, 6].map(n => board[n === 6 ? n - 1 : n][n])
+      // [0, 1, 2, 3, 4, 5, 6].map(n => {
+      //   return board[n === 0 ? 5 : 6 - n][n];
+      // })
+    ] as Row[])
+
+    .map(row => {
+      // go over the rows and select by rules
+      row.forEach((symbol, index) => {
+        if (winning.length === 4) {
+          return;
+        }
+        // check if could win
+        if (symbol && winning.length === 0) {
+          winning = [symbol];
+        } else if (symbol && winning.indexOf(symbol) !== -1) {
+          // set if symbol exists in array
+          winning.push(symbol);
+        } else if (row[index - 1] !== symbol && symbol) {
+          // otherwise set array empty
+          winning = [symbol];
+        } else {
+          // set if nothing was met
+          winning = [];
+        }
+      });
+
+      return winning;
+    });
+  console.log("return calculate winner", winning);
+  return winning[0] || null;
+};
 
 export const finished = (board: Board): boolean =>
-  board
-    .reduce((a,b) => a.concat(b) as Row)
-    .every(symbol => symbol !== null)
+  board.reduce((a, b) => a.concat(b) as Row).every(symbol => symbol !== null);
