3b6907503d0ad82937ecf4f2e73e00a687c5e7a4
gilian@kernkopje.net <gilian@kernkopje.net>
2019-05-02 18:07:09 +0200

    Implemented CSS

--- client/src/components/games/Board.js
+++ client/src/components/games/Board.js
@@ -1,19 +1,31 @@
-import React from 'react'
-import './Board.css'
+import React from "react";
+import "./Board.css";
 
 const renderCel = (makeMove, rowIndex, cellIndex, symbol, hasTurn) => {
+  let cellColor = "white";
+  if (symbol === "x") {
+    cellColor = "red";
+  } else if (symbol === "o") {
+    cellColor = "yellow";
+  }
+
   return (
-    <button
-      className="board-tile"
+    <div
+      className={cellColor}
       disabled={hasTurn}
       onClick={() => makeMove(rowIndex, cellIndex)}
       key={`${rowIndex}-${cellIndex}`}
-    >{symbol || '-'}</button>
-  )
-}
+    >
+      {/* {symbol || "-"} */}
+    </div>
+  );
+};
 
-export default ({board, makeMove}) => board.map((cells, rowIndex) =>
-  <div key={rowIndex}>
-    {cells.map((symbol, cellIndex) => renderCel(makeMove, rowIndex, cellIndex,symbol,false))}
-  </div>
-)
+export default ({ board, makeMove }) =>
+  board.map((cells, rowIndex) => (
+    <div className="boardHolderDiv" key={rowIndex}>
+      {cells.map((symbol, cellIndex) =>
+        renderCel(makeMove, rowIndex, cellIndex, symbol, false)
+      )}
+    </div>
+  ));
--- client/src/components/games/GameDetails.js
+++ client/src/components/games/GameDetails.js
@@ -1,83 +1,79 @@
-import React, {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
-import {getGames, joinGame, updateGame} from '../../actions/games'
-import {getUsers} from '../../actions/users'
-import {userId} from '../../jwt'
-import Paper from '@material-ui/core/Paper'
-import Board from './Board'
-import './GameDetails.css'
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { getGames, joinGame, updateGame } from "../../actions/games";
+import { getUsers } from "../../actions/users";
+import { userId } from "../../jwt";
+import Paper from "@material-ui/core/Paper";
+import Board from "./Board";
+import "./GameDetails.css";
 
 class GameDetails extends PureComponent {
-
   componentWillMount() {
     if (this.props.authenticated) {
-      if (this.props.game === null) this.props.getGames()
-      if (this.props.users === null) this.props.getUsers()
+      if (this.props.game === null) this.props.getGames();
+      if (this.props.users === null) this.props.getUsers();
     }
   }
 
-  joinGame = () => this.props.joinGame(this.props.game.id)
+  joinGame = () => this.props.joinGame(this.props.game.id);
 
   makeMove = (toRow, toCell) => {
-    const {game, updateGame} = this.props
+    const { game, updateGame } = this.props;
 
-    const board = game.board.map(
-      (row, rowIndex) => row.map((cell, cellIndex) => {
-        if (rowIndex === toRow && cellIndex === toCell) return game.turn
-        else return cell
+    const board = game.board.map((row, rowIndex) =>
+      row.map((cell, cellIndex) => {
+        if (rowIndex === toRow && cellIndex === toCell) return game.turn;
+        else return cell;
       })
-    )
-    updateGame(game.id, toRow, toCell)
-  }
-
-
+    );
+    updateGame(game.id, toRow, toCell);
+  };
 
   render() {
-    const {game, users, authenticated, userId} = this.props
+    const { game, users, authenticated, userId } = this.props;
 
-    if (!authenticated) return (
-			<Redirect to="/login" />
-		)
+    if (!authenticated) return <Redirect to="/login" />;
 
-    if (game === null || users === null) return 'Loading...'
-    if (!game) return 'Not found'
+    if (game === null || users === null) return "Loading...";
+    if (!game) return "Not found";
 
-    const player = game.players.find(p => p.userId === userId)
+    const player = game.players.find(p => p.userId === userId);
 
     const winner = game.players
       .filter(p => p.symbol === game.winner)
-      .map(p => p.userId)[0]
+      .map(p => p.userId)[0];
 
-    return (<Paper className="outer-paper">
-      <h1>Game #{game.id}</h1>
+    return (
+      <div>
+        <div className="gameIdMessage">Game #{game.id}</div>
 
-      <p>Status: {game.status}</p>
+        <div className="gameStatus">Status: {game.status}</div>
 
-      {
-        game.status === 'started' &&
-        player && player.symbol === game.turn &&
-        <div>It's your turn!</div>
-      }
+        {game.status === "started" && player && player.symbol === game.turn && (
+          <div>It's your turn!</div>
+        )}
 
-      {
-        game.status === 'pending' &&
-        game.players.map(p => p.userId).indexOf(userId) === -1 &&
-        <button onClick={this.joinGame}>Join Game</button>
-      }
+        {game.status === "pending" &&
+          game.players.map(p => p.userId).indexOf(userId) === -1 && (
+            <button onClick={this.joinGame}>Join Game</button>
+          )}
 
-      {
-        winner &&
-        <p>Winner: {users[winner].firstName}</p>
-      }
+        {winner && (
+          <div className="winnerMessage">
+            {users[winner].firstName} won the game!
+          </div>
+        )}
 
-      <hr />
-
-      {
-        game.status !== 'pending' &&
-        <Board board={game.board} makeMove={this.makeMove} />
-      }
-    </Paper>)
+        {game.status !== "pending" && (
+          <div className="GameBorderDiv">
+            <Board board={game.board} makeMove={this.makeMove} />
+            <br />
+            <br />
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
@@ -86,10 +82,16 @@ const mapStateToProps = (state, props) => ({
   userId: state.currentUser && userId(state.currentUser.jwt),
   game: state.games && state.games[props.match.params.id],
   users: state.users
-})
+});
 
 const mapDispatchToProps = {
-  getGames, getUsers, joinGame, updateGame
-}
+  getGames,
+  getUsers,
+  joinGame,
+  updateGame
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(GameDetails)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(GameDetails);
